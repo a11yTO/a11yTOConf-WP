346d6d4b1212179ae3ad752069431cac83296189
Lisa <lisafolkerson@gmail.com>
2017-06-21 23:09:26 -0400

    update main page content and add styles from static site

--- footer.php
+++ footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'a11ytoconf' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'a11ytoconf' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'a11ytoconf' ), 'a11ytoconf', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
+			<p><?php echo date('Y'); ?> a11yTOConf - Accessibility Toronto Conference</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
