ae7217b059c8ae525c78577682a440e6d58d1f32
Lisa <lisafolkerson@gmail.com>
2017-06-25 18:49:31 -0400

    style header nav

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,17 @@ var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
 var notify = require( 'gulp-notify' );
 
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
 gulp.task( 'styles', function(){
 	gulp.src( './styles/style.scss' )
 	.pipe( sass({ errLogToConsole : true }))
--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="description" content="An accessibility conference to connect people on digital accessibility solutions.">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ) ?>" />
 
 <?php wp_head(); ?>
 </head>
