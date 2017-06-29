631025198bc78a56041554c8572e78e703731aa9
Lisa <lisafolkerson@gmail.com>
2017-06-28 21:36:10 -0400

    acf configs for home page for home page header

--- header.php
+++ header.php
@@ -36,8 +36,10 @@
 			<?php
 			if ( is_front_page()  ) : ?>
 				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<?php $description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
+				<?php $description = get_bloginfo( 'description', 'display' ); ?>
+				<p class="date"><?php the_field('header_conf_dates'); ?></p>
+				<p class="location">Toronto, Ontario</p>
+				<?php if ( $description || is_customize_preview() ) : ?>
 					<p class="site-description  subtitle"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 				<?php
 				endif; 
